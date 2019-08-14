71ab1c4f85038dddb117bb97b73e0cb3ef29a9c4
spanda <spanda@tavisca.in>
2019-08-14 10:32:43 +0530

    Created API with asp.net empty project

---
+++ WebAPITest2/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPITest2.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Blah Blah Blah" };
+        }
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id)
+        {
+            if (id == "hello")
+                return "hi";
+            else if (id == "hi")
+                return "hello";
+            else
+                return id;
+        }
+    }
+}
